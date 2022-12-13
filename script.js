5897b4aae22eaaee466ff95f8ddcb15756eaa66a
Sullivan <adressecours0.1@gmail.com>
2022-12-13 12:26:22 +0100

    function for computer's choice created

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("Hello, world")
+
+// create a function that will randomly choose between 3 choices (orck, paper, scissors)
+
+
+let randNumber = Math.floor(Math.random() * 3);
+
+function getComputerChoice(choice){
+    if(choice === 0){
+        console.log("Rock")
+    } else if(choice === 1){
+        console.log("Paper")
+    } else {
+        console.log("Scissors")
+    }
+}
+
+console.log(getComputerChoice(randNumber));
